cdac3a3831225db6b400976ff39c46dea87e9db3
Zero <lizihevip@mail.sdu.edu.cn>
2016-10-24 16:16:59 +0800

    tcp finished

--- 环境监测系统/django/EMS/emsys/tcp(before).py
+++ 环境监测系统/django/EMS/emsys/tcp(before).py
@@ -1,35 +1,7 @@
-﻿import time
-import socket
-import threading
-#from socketserver import TCPServer, StreamRequestHandler, ForkingMixIn
+import time
+from socketserver import TCPServer, StreamRequestHandler, ForkingMixIn
 
 from data_db import Data_db
-
-#######################################################################################################################################
-#TCP服务器部分重写
-#######################################################################################################################################
-HOST = ''
-PORT = 7790
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-password = 'lizihe970106'
-
-def handle(sock):
-    while True:
-        try:
-            data = sock.recv(BUFSIZ)
-        except ConnectionResetError:
-            # 客户端因为故障等被强迫下线
-            break
-        if data.decode() == password:
-            # 这个连接就是控制器客户端
-            # 执行控制单片机客户端并反馈部分
-            # 提前可能需要一个字典或是什么的存一下各个单片机的信息，主要是设备号与socket对上
-            # 控制器客户端只允许一个人上线？
-        else:
-            # 这个连接就是单片机客户端
-            # 查一下socket这个库的具体用法，照着下面注释掉的写
-            # 执行接收数据与反馈部分
 
 #######################################################################################################################################
 #下位机数据含义解析模块，根据通讯规则制定
@@ -38,7 +10,7 @@ def handle(sock):
 def data_write(d, dev_id, date_n_time, data_list):
     for index, data in enumerate(data_list):
         time_flo = time.mktime(time.strptime(date_n_time, '%Y-%m-%d %H:%M'))+60*index
-        time_rec = time.strftime('%Y%m%d %H:%M', time.localtime(time_flo))
+        time_rec = time.strftime('%Y-%m-%d %H:%M', time.localtime(time_flo))
         tem_ori = 10*(data[0]//16)+(data[0]%16)+0.1*(data[1]//16)+0.01*(data[1]%16)
         if tem_ori > 80:
             tem = tem_ori-100
@@ -47,31 +19,24 @@ def data_write(d, dev_id, date_n_time, data_list):
         hum = 10*(data[2]//16)+(data[2]%16)+0.1*(data[3]//16)+0.01*(data[3]%16)
         pm25 = 1000*(data[4]//16)+100*(data[4]%16)+10*(data[5]//16)+(data[5]%16)
         pm10 = 1000*(data[6]//16)+100*(data[6]%16)+10*(data[7]//16)+(data[7]%16)
-        print(time_rec)
-        print(dev_id)
-        print(tem,hum,pm25,pm10)
         try:
             d.write_in(
-                    [time_rec, str(dev_id)],
-                    [str(tem), str(hum), str(pm25), str(pm10)],
+                    [time_rec, dev_id],
+                    [tem, hum, pm25, pm10],
                     )
         except:
             # 调用 Data_db 写入接口出现问题
-            return 'write error'
+            return 4
         else:
             # 成功
-            return 'okwrite SUC'
+            return 0
 
 #数据接收保存
 def data_save(d, data):
-    total = len(data)
-    print(("total length is %d") %(total))
-    #判断起始位和结束位
-    if(total==1):
-        return 'E0'
-    elif (data[0] == 13) & (data[1] == 10) & (data[-2] == 13) & (data[-1] == 10):
+    total = len(data_save)
+    if (data[0] == 13) & (data[1] == 10) & (data[-2] == 13) & (data[-1] == 10):
         dev_id = (data[2]//16)*10+(data[2]%16)
-        date_n_time = '{0}{1}{2}{3}-{4}{5}-{6}{7} {8}{9}:{10}{11}'.format(
+        date_n_time = '{0}[1][2][3]-{4}{5}-{6}{7} {8}{9}:{10}{11}'.format(
                     data[3]//16,
                     data[3]%16,
                     data[4]//16,
@@ -85,9 +50,8 @@ def data_save(d, data):
                     data[8]//16,
                     data[8]%16,
                     )
-        #如果只有一个数据(data[3]高八位和低八位表示数据)
-        if total == 20:
-            data_to_save = [data[10:18],]
+        if total == 19:
+            data_to_save = [data[9:17],]
         elif (total-12)%8 == 0:
             if (total-12)/8 == 10*(data[9]//16)+(data[9]%16):
                 data_to_save = []
@@ -95,33 +59,20 @@ def data_save(d, data):
                     data_to_save.append(data[8*i+10:8*i+18])
             else:
                 # 显示的应该包含的数据数与实际接收到的数据数不符
-                return 'F2'
+                return 2
         else:
             # 接收到的数据量既不是一个数据也不是多个数据
-            return 'F1'
-        print(dev_id)
-        print(date_n_time)
-        print(data_to_save)
+            return 3
         m = data_write(d, dev_id, date_n_time, data_to_save)
         return m
-    #下位机特殊请求
-    elif(data[0] == 10)&(data[1] == 13) & (data[-2] == 13)& (data[-1] == 10):
-        print(data[2],data[3],total)
-        #请求时间
-        if(data[2] == 87) & (data[3] == 84) & (total==6):
-            time_str = time.strftime("%Y%m%d%H%M%S",time.localtime())
-            return '\r\nWT' + time_str + '\r\n'
-        else:
-            return 'F3'
     else:
         # 不是以回车换行开头结尾，为干扰数据，直接丢弃。
-        return 'F0'
+        return 1
 
 #######################################################################################################################################
 #TCP服务器部分，直接调用Data_db的写入接口
 #使用分叉处理多个客户端同时连接
 #######################################################################################################################################
-'''
 class Server(ForkingMixIn, TCPServer):
     pass
 
@@ -135,8 +86,6 @@ class Handler(StreamRequestHandler):
             if time.strftime('%H:%M', time.localtime()) == '00:02':
                 d = Data_db()
             ret_code = data_save(d, self.data)
-            print(ret_code)
             self.request.sendall(ret_code.encode())
-server = Server(('',779), Handler)
+server = Server(('',1234), Handler)
 server.serve_forever()
-'''
